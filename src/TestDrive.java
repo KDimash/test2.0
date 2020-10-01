15166d3437997faf5cd000f7f7e14b5c68f6543b
KDimash <kairatovdimash@gmail.com>
2020-10-01 17:32:40 +0600

    test
    test2.0

---
+++ src/TestDrive.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class TestDrive {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        if(a>=1){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
